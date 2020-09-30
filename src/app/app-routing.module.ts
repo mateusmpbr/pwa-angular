11a3c88349db10bd6601902fedfd12f7817b45a3
Mateus Pereira <mateusmartinspereira@gmail.com>
2020-09-29 22:50:26 -0300

    PWA e Service Workers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListarSegurosComponent } from './components/listar-seguros/listar-seguros.component'
+import { CadastroSeguroComponent } from './components/cadastro-seguro/cadastro-seguro.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'cadastro'},
+  {path: 'cadastro', component: CadastroSeguroComponent},
+  {path: 'listar', component: ListarSegurosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
