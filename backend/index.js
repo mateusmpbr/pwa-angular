98ad586c026bef5c2a44b231cab96ae72e805ff3
Mateus Pereira <mateusmartinspereira@gmail.com>
2020-09-30 16:46:30 -0300

    Push Notifications

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,19 @@ const express = require("express");
 const {json} = require('body-parser');
 const cors = require('cors');
 const {salvarSeguro, listarSeguros} = require('./seguro-service');
+const webpush = require('web-push');
+
+// VAPID keys should only be generated only once.
+const vapidKeys = {
+    publicKey: 'BHa3WrNdFbimLPDo2_LDfacjwfTc20FK4dwzdbOIe5vW2cBGaDqRtIjpFHhvP9suA2yMW_cPqF-cBn0f4GWwxjI',
+    privateKey: 'tNiI5iBRWTp2yS4jr37-CfA3TyVc9szuXHlQq8XcVkA'
+}
+
+webpush.setVapidDetails(
+    'test@gmail.com',
+    vapidKeys.publicKey,
+    vapidKeys.privateKey
+)
 
 const app = express();
 app.use(json());
